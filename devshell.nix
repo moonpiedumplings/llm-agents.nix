ef42affbb7ab3db9330fdc9ce28dd82d84e8416c
Oleksii Filonenko <github@brightone.cloud>
2026-06-25 18:24:46 +0100

    devshell: unpin nix-update

--- devshell.nix
+++ devshell.nix
@@ -9,17 +9,7 @@ pkgs.mkShellNoCC {
     pkgs.gnugrep
     pkgs.gnused
     pkgs.jq
-    # Pin nix-update 1.16.0 for buildDotnetModule fetch-deps flake support
-    # (https://github.com/Mic92/nix-update/pull/615). Remove once nixpkgs catches up.
-    (pkgs.nix-update.overrideAttrs (_: {
-      version = "1.16.0";
-      src = pkgs.fetchFromGitHub {
-        owner = "Mic92";
-        repo = "nix-update";
-        rev = "v1.16.0";
-        hash = "sha256-LT66e5NtAJRp0E8QXKeePdTCNpH+CMvJNF1ayzBr4rw=";
-      };
-    }))
+    pkgs.nix-update
     pkgs.nodejs
 
     # Formatter
